4f8c6f2f4bd05421b2c109911b1691de156f57d3
Kamila Szewczyk <k@iczelia.net>
2025-11-05 20:24:55 +0100

    improve rng chakra #2

--- spidersol/deal.c
+++ spidersol/deal.c
@@ -7,16 +7,17 @@
 
 #include "mt19937.h"
 
-static int dealX, dealY, row_dealt;
+static int dealX, dealY, row_dealt, card0, card1;
 
 static void shuffle_card(int * dsuit, int * dvalue) {
-    int suit, value;
+    int suit, value = -1;
 
     if (game.difficulty == 3) {
         // Four suits.
         for (;;) {
             suit = genrand() % 4;
-            value = (genrand() >> 7) % 13;
+            while (value == card0 || value == card1) value = (genrand() >> 7) % 13;
+            card1 = card0; card0 = value;
             if (game.counts[suit][value] < 2) {
                 game.counts[suit][value]++;
                 break;
@@ -26,7 +27,8 @@ static void shuffle_card(int * dsuit, int * dvalue) {
         // Two suits: need to be careful. Suit must be either 1 or 2, we're simulating 4 decks.
         for (;;) {
             suit = (genrand() % 2) + 1;
-            value = (genrand() >> 7) % 13;
+            while (value == card0 || value == card1) value = (genrand() >> 7) % 13;
+            card1 = card0; card0 = value;
             if (game.counts[suit][value] < 4) {
                 game.counts[suit][value]++;
                 break;
@@ -36,7 +38,8 @@ static void shuffle_card(int * dsuit, int * dvalue) {
         // One suit: Suit must be 3, we're simulating 8 decks.
         for (;;) {
             suit = 3;
-            value = (genrand() >> 7) % 13;
+            while (value == card0 || value == card1) value = (genrand() >> 7) % 13;
+            card1 = card0; card0 = value;
             if (game.counts[suit][value] < 8) {
                 game.counts[suit][value]++;
                 break;
